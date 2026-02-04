e0d583aefa880624e38e87fc5a6cb216612190b7
AmeyAm <1230amey01@gmail.com>
2026-02-04 11:01:23 +0530

    structs

---
+++ arrayofstructs.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+typedef struct {
+    char model[25];
+    int year;
+    int price;
+}Car;
+
+int main(){
+
+    Car cars[] = {{"Mustang", 2025, 32000},
+                  {"Corvette", 2026, 68000},
+                  {"Challenger", 2025, 28000}};
+
+    int number = sizeof(cars) / sizeof(cars[0]);
+
+    for (int i = 0; i < number; i++) {
+        printf("%s %d $%d\n", cars[i].model, cars[i].year, cars[i].price);
+    }
+   return 0;
+}
