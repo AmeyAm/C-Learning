5b0cb2a0a08c54675e0529c69ef9b0b5ab2835e7
AmeyAm <1230amey01@gmail.com>
2026-02-05 11:11:25 +0530

    pointer

---
+++ pointer.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void birthday(int* age);
+
+int main(){
+
+    int age = 25;
+    int* pAge = &age;
+
+
+    birthday(pAge);
+
+    printf("you are %d years old", age);
+    
+    return 0;
+}
+void birthday(int* age){
+    (*age)++;
+}
